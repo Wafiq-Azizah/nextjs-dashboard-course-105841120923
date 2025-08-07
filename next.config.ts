7f51b2e71d4177022b7fd3f97305df01160f09b3
muhammadrizalharis <150011851+muhammadrizalharis@users.noreply.github.com>
2025-08-07 23:32:52 +0800

    Fix bangetminiiini: correct searchParams type in invoices page

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,9 @@
-// import type { NextConfig } from 'next';
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: 'incremental', // Enable partial prerendering
-  },
+  /* config options here */
 };
 
 export default nextConfig;
+
+
